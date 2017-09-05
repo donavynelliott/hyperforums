494aae97a2c490c7e47499f0f73ee0a882b58773
= <=>
2017-09-04 18:05:54 -0700

    Created User Profile Test

---
+++ app/tests/Browser/ProfileTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ProfileTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testUserProfiles()
+    {
+        $user = factory('App\User')->create();
+        $user_id = $user->id;
+
+        $this->browse(function (Browser $browser) use ($user, $user_id) {
+            $browser->visit("/profile/${user_id}")
+                    ->assertSee($user->name);
+        });
+    }
+}
