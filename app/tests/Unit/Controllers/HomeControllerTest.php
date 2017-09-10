d804cd9f8e3fed109a5b462c53a3f03473ac39fb
donavyn.elliott <donavyn.elliott@gmail.com>
2017-09-10 16:45:59 -0700

    Home Controller Test

--- app/tests/Unit/Controllers/HomeControllerTest.php
+++ app/tests/Unit/Controllers/HomeControllerTest.php
@@ -7,13 +7,25 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class HomeController extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+	public function setUp()
+	{
+		parent::setUp();
+		$this->user = factory('App\User')->create();
+	}
+	public function testHomeControllerIndexForAuthUser()
+	{	
+		$user = $this->user;
+		$response = $this->actingAs($user)
+					->get('/home');
+
+		$response->assertViewIs('home');
+	}
+
+	public function testHomeControllerIndexRedirectsToLoginForAnonUsers()
+	{
+		$response = $this->get('/home');
+
+		$response->assertStatus(302)
+				->assertRedirect('/login');
+	}
 }
