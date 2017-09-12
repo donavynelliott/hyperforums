d14a79d4031806a73a596d921050b825709b8849
Donavynelliott <donavyn.elliott@gmail.com>
2017-09-12 10:42:43 -0700

    Entire Application updated to Bootstrap 4.0

--- app/resources/views/profile.blade.php
+++ app/resources/views/profile.blade.php
@@ -4,10 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ $user->name }}'s Profile</div>
-
-                <div class="panel-body">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">{{ $user->name }}'s Profile</h4>
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
